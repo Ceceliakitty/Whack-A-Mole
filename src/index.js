a2f0d7ddd8bf215a083ff7e8583c03e280b2c1c0
Ceceliakitty <cecemcnelly@gmail.com>
2023-10-23 20:12:24 -0700

    update index.js

--- src/index.js
+++ src/index.js
@@ -5,7 +5,7 @@ const startButton = document.querySelector("#start");
 const score = document.querySelector("#score"); // Use querySelector() to get the score element
 const timerDisplay = document.querySelector("#timer"); // use querySelector() to get the timer element.
 
-let time = 0;
+let time = 10;
 let timer;
 let lastHole = 0;
 let points = 0;
